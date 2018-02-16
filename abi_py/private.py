b499e38d5f9e32ca65f3ead64515d2cb88a24ae2
yili_dong <Yili_Dong@n.f.rd.honda.co.jp>
2018-02-16 15:28:51 +0900

    add private

--- abi_py/private.py
+++ abi_py/private.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
 # coding=utf-8
 
-from tradingapis.bittrex_api import bittrex
-from tradingapis.huobi_api import huobi
 from tradingapis.bitflyer_api import pybitflyer
-from tradingapis.bitbank_api import public_api
-from tradingapis.zaif_api.impl import ZaifPublicApi,ZaifTradeApi
+from tradingapis.bitbank_api import public_api, private_api
+from tradingapis.zaif_api.impl import ZaifPublicApi, ZaifTradeApi
 from tradingapis.quoine_api import client
-# from tradingapis.quoine_api.client import Quoinex
-from tradingapis.quoine_api import client
-
+import apis
 
 
 class AutoTrading:
-
     def __init__(self):
         print("Initializing API")
-        self.bitflyer_api=pybitflyer.API(api_key="xxx...", api_secret="yyy...")
+        self.bitflyer_api = pybitflyer.API(api_key="xxx...", api_secret="yyy...")
         self.zaif_api = ZaifTradeApi(key="xxx...", secret="yyy...")
         self.quoinex_api = client.Quoinex(api_token_id="xxx...", api_secret="yyy...")
+        self.bitbank_api = private_api.bitbankcc_private(api_key="xxx...", api_secret="yyy...")
 
-
-    def trade_bitflyer(self, type, size=0.01):
+    def trade_bitflyer(self, type, amount=0.01):
         print("trade bitflyer")
-        if type == "BUY":
+        if type == "BUY" or type == "buy":
             self.bitflyer_api.sendchildorder(product_code="BTC_JPY",
                                              child_order_type="MARKET",
                                              side="BUY",
-                                             size=0.01,
+                                             size=amount,
                                              minute_to_expire=10000,
                                              time_in_force="GTC"
                                              )
-        elif type == "SELL":
+        elif type == "SELL" or type == "sell":
             self.bitflyer_api.sendchildorder(product_code="BTC_JPY",
                                              child_order_type="MARKET",
                                              side="SELL",
@@ -42,47 +37,63 @@ class AutoTrading:
         else:
             print("error!")
 
-
-    def trade_quoniex(self,type, size=0.01):
+    def trade_quoniex(self, type, amount=0.01):
         print("trade_quoniex")
         products = self.quoinex_api.get_products()
 
-        if type=="BUY":
+        if type == "BUY" or type == "buy":
             order = self.quoinex_api.create_market_buy(
                 product_id=products[5]['id'],
-                quantity=str(size))
-        elif type=="SELL":
+                quantity=str(amount))
+        elif type == "SELL" or type == "sell":
             order = self.quoinex_api.create_market_sell(
                 product_id=products[5]['id'],
-                quantity=str(size))
+                quantity=str(amount))
+        else:
+            print("error!")
+
+    def trade_zaif(self, type, amount=0.01):
+        print("trade_zaif")
+
+        margin_ratio = 0.1
+        [bid, ask] = apis.get_bid_ask_zaif('btc_jpy')
+
+        if type == "BUY" or type == "buy":
+            self.zaif_api.trade(
+                currency_pair='btc_jpy',
+                action='ask',
+                price=ask * (1 + margin_ratio),
+                amount=amount
+            )
+        elif type == "SELL" or type == "sell":
+            self.zaif_api.trade(
+                currency_pair='btc_jpy',
+                action='bid',
+                price=bid * (1 - margin_ratio),
+                amount=amount
+            )
+        else:
+            print("error!")
+
+    def trade_bitbank(self, type, amount=0.01):
+        print("trade_bitbank")
+        margin_ratio = 0.1
+        [bid, ask] = apis.get_bid_ask_bitbank('btc_jpy')
+        if type == "BUY" or type == "buy":
+            self.bitbank_api.order(pair='btc_jpy',
+                                   price=str(ask * (1 + margin_ratio)),
+                                   amount=str(amount),
+                                   side='buy',
+                                   order_type="market")
+        elif type == "SELL" or type == "sell":
+            self.bitbank_api.order(pair='btc_jpy',
+                                   price=str(bid * (1 - margin_ratio)),
+                                   amount=str(amount),
+                                   side='sell',
+                                   order_type='market')
         else:
             print("error!")
 
 
-    def trade_zaif(self):
-        print("trade_zaif")
-
-        zaifpublicApi = ZaifPublicApi()
-        # zaifpublicApi.last_price ('btc_jpy')
-
-        jsons_dict = zaifpublicApi.ticker('btc_jpy')
-        bid = jsons_dict['result']['Bid']
-        ask = jsons_dict['result']['Ask']
-
-        self.zaif_api.trade(
-            currency_pair='btc_jpy',
-            action='bid',
-            price=500000,
-            amount=0.8
-        )
-
-    def trade_bitbank(self):
-        print("trade_bitbank")
-
-
-
 if __name__ == '__main__':
     print("Shooting")
-
-
-
