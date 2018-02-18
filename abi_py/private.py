c909755282fd70add97e4db85a1a7136cb88888c
yilidong <dongyili2004@gmail.com>
2018-02-18 14:03:55 +0900

    modify private.py

--- abi_py/private.py
+++ abi_py/private.py
@@ -31,21 +31,21 @@ class AutoTrading:
             self.bitflyer_api.sendchildorder(product_code="BTC_JPY",
                                              child_order_type="MARKET",
                                              side="SELL",
-                                             size=0.01,
+                                             size=amount,
                                              minute_to_expire=10000,
                                              time_in_force="GTC"
                                              )
         else:
             print("error!")
 
-    def trade_quoniex(self, type, amount=0.001):
+    def trade_quoinex(self, type, amount=0.001):
         print("trade_quoniex")
         products = self.quoinex_api.get_products()
-        pid=5
+        pid = 5
         for product in products:
             # print(product['currency_pair_code'])
-            if product['currency_pair_code']=='BTCJPY':
-                pid=int(product['id'])
+            if product['currency_pair_code'] == 'BTCJPY':
+                pid = int(product['id'])
 
         # print(products[4])
         print(pid)
@@ -67,11 +67,11 @@ class AutoTrading:
         # [bid, ask] = apis.get_bid_ask_zaif('btc_jpy')
         # bid = float(bid)
         # ask = float(ask)
-        zaifpublic=ZaifPublicApi()
+        zaifpublic = ZaifPublicApi()
         last_price = int(zaifpublic.last_price(currency_pair='btc_jpy')["last_price"])
 
         if type == "BUY" or type == "buy":
-            price=int(last_price * (1 + margin_ratio))
+            price = int(last_price * (1 + margin_ratio))
             self.zaif_api.trade(
                 currency_pair='btc_jpy',
                 action='bid',
@@ -109,68 +109,145 @@ class AutoTrading:
             print("error!")
 
     def get_asset_bitbank(self):
-        balances=self.bitbank_api.get_asset()
+        balances = self.bitbank_api.get_asset()
         jpy_avai = 0.0
         btc_avai = 0.0
         for balance in balances['assets']:
-            if balance['asset']=='jpy':
-                jpy_avai =balance['onhand_amount']
-            if balance['asset']=='btc':
-                btc_avai =balance['onhand_amount']
+            if balance['asset'] == 'jpy':
+                jpy_avai = float(balance['onhand_amount'])
+            if balance['asset'] == 'btc':
+                btc_avai = float(balance['onhand_amount'])
         return ([jpy_avai, btc_avai])
 
     def get_asset_bitflyer(self):
-        balances=self.bitflyer_api.getbalance(product_code="BTC_JPY")
+        balances = self.bitflyer_api.getbalance(product_code="BTC_JPY")
         print(balances)
-        jpy_avai=0.0
-        btc_avai=0.0
+        jpy_avai = 0.0
+        btc_avai = 0.0
         for balance in balances:
             if balance['currency_code'] == 'JPY':
-                jpy_avai=balance['available']
+                jpy_avai = float(balance['available'])
             elif balance['currency_code'] == 'BTC':
-                btc_avai = balance['available']
+                btc_avai = float(balance['available'])
         print(jpy_avai)
         print(btc_avai)
         return ([jpy_avai, btc_avai])
 
     def get_asset_quoinex(self):
-        balances=self.quoinex_api.get_account_balances()
+        balances = self.quoinex_api.get_account_balances()
         jpy_avai = 0.0
         btc_avai = 0.0
         for balance in balances:
             if balance['currency'] == 'BTC':
-                btc_avai = balance['balance']
+                btc_avai = float(balance['balance'])
             elif balance['currency'] == 'JPY':
-                jpy_avai = balance['balance']
+                jpy_avai = float(balance['balance'])
 
         return ([jpy_avai, btc_avai])
 
-
     def get_asset_zaif(self):
-        infos=self.zaif_api.get_info()
-        btc_avai=infos['funds']['btc']
-        jpy_avai = infos['funds']['jpy']
-        return ([jpy_avai,btc_avai])
+        infos = self.zaif_api.get_info()
+        btc_avai = float(infos['funds']['btc'])
+        jpy_avai = float(infos['funds']['jpy'])
+        return ([jpy_avai, btc_avai])
 
     def execute_trade(self, bankname, action, amount):
-        print("trade")
-        # to do here
+        print("execute_trade")
+        if not(self.judge_asset(bankname,action,amount)):
+            return False
+
+        if bankname == "quoinex":
+            self.trade_quoinex(action, amount)
+        elif bankname == "bitbank":
+            self.trade_bitbank(action, amount)
+        elif bankname == "bitflyer":
+            self.trade_bitflyer(action, amount)
+        elif bankname == "zaif":
+            self.trade_zaif(action, amount)
+
+        return True
+
+    def judge_asset(self,bankname, action, amount):
+        if not(self.check(bankname, action, amount)):
+            return False
+
+        [jpy_avai, btc_avai] = [0. , 0.]
+        [ask, bid] = [0., 0.]
+        if bankname == "quoinex":
+            [jpy_avai, btc_avai]=self.get_asset_quoinex()
+            # [ask, bid] = apis.get_bid_ask_quoinex()
+            [ask, bid] == [1000000000,100000000]
+        elif bankname == "bitbank":
+            [jpy_avai, btc_avai] = self.get_asset_bitbank()
+            [bid, ask] = apis.get_bid_ask_bitbank('BTC_JPY')
+        elif bankname == "bitflyer":
+            [jpy_avai, btc_avai] = self.get_asset_bitflyer()
+            [bid, ask] = apis.get_bid_ask_bitflyer('BTC_JPY')
+        elif bankname == "zaif":
+            [jpy_avai, btc_avai] = self.get_asset_zaif()
+            [bid, ask] = apis.get_bid_ask_zaif('BTC_JPY')
+
+        if action=="buy" or action=="BUY":
+            if amount*ask > jpy_avai*1.02:
+                print ("Buy power is not enough!")
+                return False
+        elif action=="sell" or action=="SELL":
+            if amount > btc_avai*0.99:
+                print("Sell power is not enough!")
+                return False
+
+        return True
+
+    def check(self,bankname, action, amount):
+        if action != "buy" and action != "sell" and action != "BUY" and action != "SELL":
+            print("Action invalid!")
+            return False
+
+        if bankname !="quoinex" and bankname !="zaif" and bankname !="bitbank" and bankname !="bitflyer":
+            print ("Bankname invalid!")
+            return False
+
+        if amount < 0.0:
+            print("Amount invalid!")
+            return False
+
+        return True
+
 
 class Arbitrage:
     def __init__(self):
-        print("Initializing Arbitrage")
-        self.autotrade=AutoTrading()
+        print("Initializing Arbitrage"
+              ""
+              "")
+        self.autotrade = AutoTrading()
 
-    def arbitrage_once(self, buy_bankname, sell_bankname, amount):
+    def arbitrage_once(self, buy_bankname, sell_bankname, amount=0.001):
         print("arbitrage_once")
         # to do here
+        if not(self.autotrade.judge_asset(buy_bankname,"buy", amount)):
+            return False
+
+        if not(self.autotrade.judge_asset(sell_bankname,"sell", amount)):
+            return False
+
+        self.autotrade.execute_trade(buy_bankname,"buy",amount)
+        self.autotrade.execute_trade(sell_bankname, "sell", amount)
+        return True
 
 
 
 if __name__ == '__main__':
     print("Shooting")
-    mytrade=AutoTrading()
+    mytrade = AutoTrading()
     # mytrade.get_asset_bitflyer()
-    print(mytrade.get_asset_zaif())
+    print(mytrade.get_asset_bitflyer())
 
-    mytrade.trade_zaif(type="sell", amount=0.001)
+    # mytrade.trade_zaif(type="sell", amount=0.001)
+
+    myarbitrage=Arbitrage()
+
+    if myarbitrage.arbitrage_once("quoinex","bitflyer",0.002) ==True:
+        print("Successfully Executed!")
+
+    # mytrade.trade_bitflyer("sell", amount=0.002)
+
